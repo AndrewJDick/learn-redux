867fc515749a400f54c267720d75fb645b3f9dc8
Andrew James Dick <andrew.dick@cohaesus.co.uk>
2017-03-07 11:54:29 +0000

    Video 9: Added console log to show that every reducer will run when an action is dispatched

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,9 +1,15 @@
+// Whenever any action is dispatched, every reducer will run. 
+
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
 import posts from './posts';
 import comments from './comments';
 
-const rootReducer = combineReducers({ posts, comments, routing: routerReducer });
+const rootReducer = combineReducers({ 
+	posts, 
+	comments, 
+	routing: routerReducer 
+});
 
 export default rootReducer;
--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -3,6 +3,7 @@
 // 2) Copy of the current state 
 
 function posts (state = [], action) {
+	console.log('This post will change');
 	console.log(state, action);
 	return state;
 }
